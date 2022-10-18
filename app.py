a87b3ae983aa63d74446c723f1eb94c4c4482dd4
Essteel <siannon.steel@gmail.com>
2022-10-18 11:42:31 +1030

    added user class

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:passwo
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+
 class Card(db.Model):
     __tablename__ = 'cards'
     id = db.Column(db.Integer, primary_key=True)
@@ -39,40 +48,40 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
-        cards = [
-            Card(
-                title = 'Start the project',
-                description = 'Stage 1 - Create the database',
-                status = 'To Do',
-                priority = 'High',
-                date = date.today()
-            ),
-            Card(
-                title = "SQLAlchemy",
-                description = "Stage 2 - Integrate ORM",
-                status = "Ongoing",
-                priority = "High",
-                date = date.today()
-            ),
-            Card(
-                title = "ORM Queries",
-                description = "Stage 3 - Implement several queries",
-                status = "Ongoing",
-                priority = "Medium",
-                date = date.today()
-            ),
-            Card(
-                title = "Marshmallow",
-                description = "Stage 4 - Implement Marshmallow to jsonify models",
-                status = "Ongoing",
-                priority = "Medium",
-                date = date.today()
-            )
-        ]
+    cards = [
+        Card(
+            title = 'Start the project',
+            description = 'Stage 1 - Create the database',
+            status = 'To Do',
+            priority = 'High',
+            date = date.today()
+        ),
+        Card(
+            title = "SQLAlchemy",
+            description = "Stage 2 - Integrate ORM",
+            status = "Ongoing",
+            priority = "High",
+            date = date.today()
+        ),
+        Card(
+            title = "ORM Queries",
+            description = "Stage 3 - Implement several queries",
+            status = "Ongoing",
+            priority = "Medium",
+            date = date.today()
+        ),
+        Card(
+            title = "Marshmallow",
+            description = "Stage 4 - Implement Marshmallow to jsonify models",
+            status = "Ongoing",
+            priority = "Medium",
+            date = date.today()
+        )
+    ]
 
-        db.session.add_all(cards)
-        db.session.commit()
-        print('Tables seeded')
+    db.session.add_all(cards)
+    db.session.commit()
+    print('Tables seeded')
 
 # Old way LEGACY
 # @app.cli.command('all_cards')
